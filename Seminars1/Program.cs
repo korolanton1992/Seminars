630e139da40d63894d65d69d4cdcb11fe2109026
Anton <toscha.korol@yandex.ru>
2023-04-25 20:47:35 +0300

    Task3

---
+++ Seminars1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Console.WriteLine("input number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ($"You input {{{(num)}}} number"); 
+
+// Task1. Запросить число и вывести его квадрат.
+
+// Console.WriteLine("input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quad of {num} -> {num*num}");
+
+// Task2. Является ли первое число квадратом второго?
+
+// Console.WriteLine("input first number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second numder");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num == num2*num2)
+// {
+//     Console.WriteLine($"{num} является квадратом {num2}");
+// }
+// else
+// Console.WriteLine($"{num} не является квадратом {num2}");
+
+// Task3. Напишите програма которая принимает на вход число N. На выходе получаем последовательность от -N до N.
+
+Console.WriteLine ("input N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n<0) Console.Write("invalid input N");
+
+int Count = n*(-1);
+while(Count <= n)
+{
+    Console.Write(Count+" ");
+    Count++;
+}
