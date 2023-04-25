bfde9fab623c7d088821f825603dce297c40dd2e
Anton <toscha.korol@yandex.ru>
2023-04-25 21:04:21 +0300

    Task4

--- Seminars1/Program.cs
+++ Seminars1/Program.cs
@@ -24,14 +24,22 @@
 
 // Task3. Напишите програма которая принимает на вход число N. На выходе получаем последовательность от -N до N.
 
-Console.WriteLine ("input N");
+// Console.WriteLine ("input N");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if(n<0) Console.Write("invalid input N");
+
+// int Count = n*(-1);
+// while(Count <= n)
+// {
+//     Console.Write(Count+" ");
+//     Count++;
+// }
+
+// Task 4. На вход принимаем трехзначенное число. Вывести последнюю цифру данного числа.
+
+Console.WriteLine ("input three-digit number");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if(n<0) Console.Write("invalid input N");
-
-int Count = n*(-1);
-while(Count <= n)
-{
-    Console.Write(Count+" ");
-    Count++;
-}
+int ed = n % 10;
+Console.WriteLine($"Last digit of {n} -> {ed}");
